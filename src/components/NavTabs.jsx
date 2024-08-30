9632f930394590ec182da60de3cf7c138187385d
unknown <rosa.angel.daniel@gmail.com>
2024-08-30 17:21:19 -0600

    completed contents in Navtabs

--- src/components/NavTabs.jsx
+++ src/components/NavTabs.jsx
@@ -5,6 +5,20 @@ function NavTabs({ currentPage, handlePageChange }) {
                 <a href="#AboutMe" onClick={() => handlePageChange('AboutMe')} className={currentPage === 'AboutMe' ? 'nav-link active' : 'nav-link'}>About Me</a>
             </li>
 
+            <li className="nav-item">
+                <a href="#Resume" onClick={() => handlePageChange('Resume')} className={currentPage === 'Resume' ? 'nav-link active' : 'nav-link'}>Resume</a> 
+            </li>
+
+            <li className="nav-item">
+                <a href="#Portfolio" onClick={() => handlePageChange('Portfolio')} className={currentPage === 'Portfolio' ? 'nav-link active' : 'nav-link'}>Portfolio</a>
+            </li>
+
+            <li className="nav-item">
+                <a href="#Contact" onClick={() => handlePageChange('Contact')} className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}>Contact</a>
+            </li>
+
         </ul>
-    )
+    );
 }
+
+export default NavTabs;
