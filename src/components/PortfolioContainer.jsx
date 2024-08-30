ade9f291d415bb826fd4e94f08e91718a3a0821d
unknown <rosa.angel.daniel@gmail.com>
2024-08-30 16:26:21 -0600

    completed PortfolioContainer file

--- src/components/PortfolioContainer.jsx
+++ src/components/PortfolioContainer.jsx
@@ -1,2 +1,32 @@
 import { useState } from 'react';
 import NavTabs from './NavTabs';
+import AboutMe from './pages/AboutMe';
+import Resume from './pages/Resume';
+import Portfolio from './pages/Portfolio';
+import Contact from './pages/contact';
+
+export default function PortfolioContainer() {
+    const [currentPage, setCurrentPage] = useState('AboutMe');
+
+    const renderPage = () => {
+        if (currentPage === 'AboutMe') {
+            return <AboutMe />;  
+        }
+        if (currentPage === 'Resume') {
+            return <Resume />;
+        }
+        if (currentPage === 'Portfolio') {
+            return <Portfolio />;
+        }
+            return <Contact />;
+    };
+
+    const handlePageChange = (page) => setCurrentPage(page);
+
+    return (
+        <div>
+            <NavTabs currentPage={currentPage} handlePageChange={handlePageChange} /> 
+            <main className='mx=3'>{renderPage()}</main>
+        </div>
+    );
+ }
