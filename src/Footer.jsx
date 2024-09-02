0e49558968ed35c10e501d97b0e2ae7aff9ad360
unknown <rosa.angel.daniel@gmail.com>
2024-09-01 18:52:36 -0600

    completed footer

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -5,14 +5,10 @@ function Footer() {
         <footer style={footerStyle}>
         <div className="container">
             <p>&copy; {new Date().getFullYear()} Angel Fernandez. All Rights Reserved.</p>
-            <p>Contact us: <a href="mailto:example@example.com" className="text-white">example@example.com</a></p>
-            
-            {/* Social Media Icons */}
+            <p>Contact Me: <a href="" className="text-white">rosa.angel.daniel@gmail.com</a>
+            </p>
             <div className="mt-3">
-                <a href="https://twitter.com/your-twitter-profile" target="_blank" rel="noopener noreferrer" className="text-white mx-2">
-                    <i className="fab fa-twitter fa-2x"></i>
-                </a>
-                <a href="https://github.com/your-github-profile" target="_blank" rel="noopener noreferrer" className="text-white mx-2">
+                <a href="https://github.com/b3hold23" target="_blank" rel="noopener noreferrer" className="text-white mx-2">
                     <i className="fab fa-github fa-2x"></i>
                 </a>
             </div>
