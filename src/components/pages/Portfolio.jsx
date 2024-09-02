c1e7c33569d4b529982e7942847f3641b2652234
unknown <rosa.angel.daniel@gmail.com>
2024-09-01 21:05:21 -0600

    added images to the portfolio page

--- src/components/pages/Portfolio.jsx
+++ src/components/pages/Portfolio.jsx
@@ -5,13 +5,20 @@ export default function Portfolio() {
         {/* Add clickable links to external websites */}
         <ul>
             <li>
-                <a href="https://www.example.com" target="_blank" rel="noopener noreferrer">Visit Example</a>
+                <img className="fit-picture" src="Portfolio-Assets/command-line.JPG" alt="ReadMe Creator img"  />
+                <a href="https://github.com/b3hold23/readme-creator" target="_blank" rel="noopener noreferrer">Readme Creator</a>
             </li>
             <li>
-                <a href="https://www.anotherexample.com" target="_blank" rel="noopener noreferrer">Visit Another Example</a>
+                <img className="fit-picture" src="Portfolio-Assets/screenshot.png" alt="Car loan payment website"/> 
+                <a href="https://github.com/b3hold23/car-loan-payment" target="_blank" rel="noopener noreferrer">Car Loan Payment</a>
             </li>
             <li>
-                <a href="https://www.github.com" target="_blank" rel="noopener noreferrer">My GitHub Profile</a>
+                <img className="fit-picture" src="Portfolio-Assets/Completed site.JPG" alt="Weather Dashboard website homepage"/> 
+                <a href="https://github.com/b3hold23/weather-app" target="_blank" rel="noopener noreferrer">Weather App</a>
+            </li>
+            <li>
+                <img className="fit-picture" src="Portfolio-Assets/employee-tracker.JPG" />
+                <a href="https://github.com/b3hold23/employee-tracker" target="_blank" rel="noopener noreferrer">Employee Tracker</a>
             </li>
         </ul>
     </div>
