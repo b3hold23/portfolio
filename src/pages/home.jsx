d919798ad2f3fa0eed5cdcbbe57a31053679336c
unknown <rosa.angel.daniel@gmail.com>
2025-02-19 20:51:07 -0700

    completed homepage

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,16 +1,27 @@
-// import Pfp from '../assets/portfolio-pfp.jpg';
+import Pfp from '../assets/portfolio-pfp.jpg';
+
+
 const Home = () => {
     return (
-        <div className="container">
-            <div className="row">
-                <div className="col">
-                    <h1>Home</h1>
-                    <p>
-                        Welcome to my portfolio! My name is John Doe and I am a web developer. 
-                        I have experience with HTML, CSS, JavaScript, and React. 
-                        I am currently learning Node.js and Express. 
-                        I am passionate about web development and I am excited to learn more about backend development.
-                    </p>
+        <div className="card border-black mt-5 bg-black">
+            <div className="row g-0">
+                <div className="col-md-4">
+                    <img src={Pfp} alt="pic" className="img-fluid rounded-start" />
+                </div>
+                <div className="col-md-8">
+                    <div className="card-body">
+                        <h5 className="card-title text-light mt-4 text-center">About Me</h5>
+                        <p className="card-text text-secondary text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi optio fuga ullam tempora harum provident recusandae repellendus incidunt rem, ipsam labore corporis asperiores quos distinctio minima deleniti similique qui culpa.</p>
+                    </div>
+                    <div className="card-body">
+                        <h5 className="card-title text-light mt-4 text-center">Skills</h5>
+                        <p className="card-text text-secondary text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi optio fuga ullam tempora harum provident recusandae repellendus incidunt rem, ipsam labore corporis asperiores quos distinctio minima deleniti similique qui culpa.</p>
+                    </div>
+                    <div className="card-body mt-5 py-5 d-grid gap-2 col-3 mx-auto">
+                        <button className="btn btn-outline-secondary bg-secondary btn-lg">
+                            <a href="/projects" className="link-body-emphasis link-offset-2 link-underline-opacity-25 link-underline-opacity-75-hover">Check out my work!</a>
+                        </button>
+                    </div>
                 </div>
             </div>
         </div>
