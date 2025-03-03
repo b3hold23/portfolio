d5c3632c0924b43cfdee145d0aa20adb57740a0d
unknown <rosa.angel.daniel@gmail.com>
2025-03-03 13:10:06 -0700

    added about me contents and minor changes to the look of the page

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -1,16 +1,13 @@
 import profile from "../../assets/portfolio-pfp.jpg";
 function About() {
   return (
-    <section className="my-5">
+    <section className="my-4">
       <div className="my-2">
         <div className="profile-img my-5">
           <img style={{width: 350, borderRadius: '50%'}} src={profile} alt="pfp" />
         </div>
-        <p>
-        Helvetica put a bird on it church-key Blue Bottle banjo bespoke brunch Etsy authentic Marfa quinoa typewriter plaid direct trade small batch wayfarers bicycle rights cliche craft beer gastropub single-origin coffee Godard Carles you probably haven't heard of them irony pickled kitsch synth sriracha gentrify literally heirloom blog Truffaut paleo scenester
-        </p>
-        <p>
-        Echo Park sriracha Pinterest vegan biodiesel 90's irony iPhone Kickstarter Carles crucifix kitsch narwhal dreamcatcher pickled trust fund selvage art party letterpress Tumblr post-ironic kogi Thundercats Tonx Brooklyn Pitchfork Odd Future authentic normcore freegan leggings Schlitz chambray organic tousled retro fap squid street art church-key fashion axe Wes Anderson bespoke whatever gentrify banh mi you probably haven't heard of them American Apparel next level pug ugh.
+        <p className="about-me">
+        Energetic and optimistic MERN full-stack developer with a background in customer service and over 5 years of experience assisting customers in solving complex issues. Angel is a dedicated problem-solver who thrives on learning new skills and growing his expertise. With a strong foundation in MongoDB, Express, React, and Node.js, he is eager to apply his technical knowledge alongside his interpersonal skills to build user-friendly applications and enhance customer experiences. In previous roles, he maintained email and call queues at record lows and piloted a new email communication service, demonstrating his initiative and efficiency. 
         </p>
       </div>
     </section>
